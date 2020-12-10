677cb8183092ff7e869660acdc2fc21bacc14e10
hiko28 <57925629+hiko28@users.noreply.github.com>
2020-12-10 11:11:33 +0900

    Add files via upload

---
+++ JSample1_6.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//delete用のコード
+public class JSample1_6 extends JFrame implements  ActionListener{
+    public static void main(String args[]){
+        JSample1_6 frame = new JSample1_6("MyTitle");
+        frame.setVisible(true);
+    }
+
+    JSample1_6(String title){
+        setTitle(title);
+        setBounds(100, 100, 600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel p = new JPanel();
+        JButton btn = new JButton("削除");
+        btn.setForeground(Color.RED);
+        //btn.setBackground(Color.RED);
+        btn.addActionListener(this);
+
+        p.add(btn);
+        getContentPane().add(p, BorderLayout.CENTER);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        JLabel label = new JLabel("削除しました");
+        JOptionPane.showMessageDialog(this, label);
+    }
+
+}
